6f18117da38bc865a2df08f44969349a95d1a3e6
bojanabb <bojana.buljic@gmail.com>
2020-07-14 11:27:36 +0200

    add RelayCommand,ViewModelBase,JMBGValidation

---
+++ DAN_XLVI_Bojana_Buljic/Zadatak_1/ViewModel/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace Zadatak_1.ViewModel
+{
+    public abstract class ViewModelBase:INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event raised when some property is changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Register each change on control/command binded for a Property
+        /// </summary>
+        /// <param name="propertyName">string parameter for a property name</param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
